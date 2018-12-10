98f9baab409c0c9c91521d668720273ca2447722
Ajay <ajaymungur@hotmail.com>
2018-12-10 17:07:26 +0000

    Added method to return string with html

--- spec/note-list-view-spec.js
+++ spec/note-list-view-spec.js
@@ -19,3 +19,22 @@ function canCreateNoteListView() {
 }
 
  canCreateNoteListView();
+
+
+ function canCreateNoteListView() {
+   var note1 = new Note("Note number 1")
+   var note2 = new Note("Note number 2")
+
+   var notelist = new NoteList();
+   notelist.addNote(note1)
+   notelist.addNote(note2)
+   notelist.createNote("First created note");
+
+
+   var noteListView = new NoteListView(notelist);
+   stringlist = noteListView.stringList();
+   console.log("stringlist is:" + stringlist)
+   assert.isTrue(stringlist === "<ul><li><div>Note number 1</div></li><li><div>Note number 2</div></li><li><div>First created note</div></li></ul>")
+ }
+
+  canCreateNoteListView();
--- src/note-list-view.js
+++ src/note-list-view.js
@@ -1,3 +1,12 @@
 var NoteListView = function(noteList) {
   this.list = noteList.list;
 }
+
+NoteListView.prototype.stringList = function () {
+  var string = "<ul>"
+  for (var i = 0; i < this.list.length; i++) {
+    string += "<li><div>" + this.list[i].text + "</div></li>"
+  }
+  console.log(string)
+  return (string += "</ul>")
+};
